3e5b5bf73d363eb475aee16d8ec5a53fbd6ad427
Declan Feehily <s00145584@mail.itsligo.ie>
2016-03-21 15:36:43 +0000

    start of local api

---
+++ RAD302CA/RAD302CA/DAL/ICoordRepository.cs
@@ -0,0 +1,14 @@
+﻿using RAD302CA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAD302CA.DAL
+{
+    interface ICoordRepository:IDisposable
+    {
+        List<CoordTbl> GetCoords();
+    }
+}
